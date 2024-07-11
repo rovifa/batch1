3fea225eb67e57a1e1e27350235d6144aaa0c9ca
ROVI <rovifauzan95@gmail.com>
2024-07-11 18:16:59 +0700

    test

--- controller/auth.js
+++ controller/auth.js
@@ -59,10 +59,13 @@ export async function login(req,res,next){
 
 export const tokenValidation = (request, response, next) => {
     try {
+        
         let authToken = request.headers.authorization;
         let accessToken = authToken && authToken.split(' ')[1];
-
+        console.log(accessToken);
         jwt.verify(accessToken, accessToken, (error, payload) => {
+            console.log(error);
+            console.log(payload);
             if (!error){
                 console.log(payload);
                 request.claims = payload;
